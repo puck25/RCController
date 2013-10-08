034af53297ff4ce724971b2e08faf62415bc9150
Keir Frampton <Keirframpton@gmail.com>
2013-10-08 23:05:12 +0100

    started to build server dashboard

--- GyroControl.js
+++ GyroControl.js
@@ -4,20 +4,19 @@ if (window.DeviceOrientationEvent) {
       var LR = eventData.gamma;
       var FB = eventData.beta;
       var DIR = eventData.alpha;
-      deviceOrientationHandler(LR, FB, DIR);
+	  ObjectMove(LR, FB, DIR);
       UpdateStats(LR, FB, DIR);
   }, false);
 } else {
         alert("Not supported on your device or browser.  Sorry.");
 }
- 
-function deviceOrientationHandler(LR, FB, DIR) {
-   //for webkit browser
-   document.getElementById("ballControll").style.webkitTransform = "rotate("+ LR +"deg) rotate3d(1,0,0, "+ (FB*-1)+"deg)";
- 
-   //for HTML5 standard-compliance
-   document.getElementById("ballControll").style.transform = "rotate("+ LR +"deg) rotate3d(1,0,0, "+ (FB*-1)+"deg)";
-}
+
+
+function ObjectMove(LR, FB, DIR){
+	var ball = document.getElementById("ballControll");
+	document.getElementById("ballControll").style.transform = "rotate("+ LR +"deg) rotate3d(1,0,0, "+ (FB*-1)+"deg)";
+};
+
 
 function UpdateStats(LR, FB, DIR){
     var alpha = document.getElementById("alpha");
