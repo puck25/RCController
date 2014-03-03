936764099990fed71541af1d2aecfa36b28b9d63
Keir Frampton <Keirframpton@gmail.com>
2014-03-03 22:31:33 +0000

    made changes to add min and max values to front steering. Also added control to stop sending to much data to the Pi. increased response times

--- GyroControl.js
+++ GyroControl.js
@@ -1,46 +1,84 @@
-if (window.DeviceOrientationEvent) {
-  console.log("DeviceOrientation is supported");
-  window.addEventListener('deviceorientation', function (eventData) {
-      var GAMMA = eventData.gamma*2;
-      var BETA = eventData.beta*8;
-      var ALPHA = eventData.alpha;
-      //ObjectMove(GAMMA, BETA, ALPHA);
-	  UpdateStats(GAMMA, BETA, ALPHA);
-	  PHPRelay(ALPHA, BETA, GAMMA);  
-  }, false);
-} else {
-        alert("Not supported on your device or browser.  Sorry!");
+var centerbeta = 100; //where is the middle
+var minbeta = '-600'; //right limit
+var maxbeta = '600'; //left limit
+var multbeta = 10; //factor to multiply the raw gyro figure by to get the desired rande of steering
+
+
+var centergamma = 100;
+var ajustmentgamma = 70; //what do we do to the angle to get to 0 an rest
+var mingamma = 250; //backwards limit
+var maxgamma = 400; //forward limit
+var multgamma = 1; //factor to multiply the raw gyro figure by to get the desired rante os accalaration
+
+window.lastbeta='0';
+window.lastgamma='0';
+
+$(function(){
+  window.gyro = 'ready';
+  alert('Ready -- Lets race !');
+});
+
+window.ondeviceorientation = function (event) {
+
+    beta = centerbeta + (Math.round(event.beta * -1) * multbeta);
+    if (beta >= maxbeta) {
+        beta = maxbeta;
+    }
+    if (beta <= minbeta) {
+        beta = minbeta;
+    }
+    gamma = event.gamma;
+    gamma = ((Math.round(event.gamma) + ajustmentgamma) * multgamma) + centergamma;
+    /*
+    if (gamma >= maxgamma) {
+        gamma = maxgamma;
+    }
+    if (gamma <= gamma) {
+        gamma = mingamma;
+    }
+    */
+
+
+
+    //stop sending the same command more than once
+    send = 'N';
+    if (window.lastbeta != beta) { send = 'Y' }
+    if (window.lastgamma != gamma) { send = 'Y' }
+
+    window.lastbeta = beta;
+    window.lastgamma = gamma;
+
+    if (window.gyro == 'ready' && send == 'Y') { //dont send another command until last one doone... its only a little pi
+        window.gyro = 'notready';
+        //  $.post("gyro.cgi", { beta: beta, gamma: gamma }, function(data) {
+        //socket.emit('fromclient', { beta: beta, gamma: gamma } );
+        window.gyro = 'ready';
+        PHPRelay(beta, gamma);
+        UpdateStats(beta, gamma);
+        //    $('#info').text('Raw Beta: '+event.beta+' Send:'+beta);
+        //    $('#info2').text('Raw Gamma: '+event.gamma+' Send:'+gamma + (Math.round(event.gamma)*multgamma));
+        //  });
+    }
 }
 
-
-/*
-function ObjectMove(GAMMA, BETA, ALPHA){
-	var ball = document.getElementById("ballControll");
-	document.getElementById("ballControll").style.webkitTransform = "rotate("+ BETA +"deg)";
-};
-*/
-
-function UpdateStats(GAMMA, BETA, ALPHA){
+function UpdateStats(GAMMA, BETA){
 	console.log("Update");
-    var alpha = document.getElementById("alpha");
     var beta = document.getElementById("beta");
     var gamma = document.getElementById("gamma");
-    $(alpha).html("Alpha: " + Math.round(ALPHA));
-    $(beta).html("Beta: " + Math.round(BETA));
-    $(gamma).html("Gamma: " + Math.round(GAMMA));
+    $(beta).html("Beta: " + BETA);
+    $(gamma).html("Gamma: " + GAMMA);
 	
 };
 
-function PHPRelay(ALPHA, BETA, GAMMA){
+function PHPRelay(BETA, GAMMA){
 	console.log("Send Data");
-    var alpharound = Math.round(ALPHA);
-    var betaaround = Math.round(BETA);
-    var gammaround = Math.round(GAMMA);
+    var betaaround = BETA;
+    var gammaround = GAMMA;
     //console.log(alpharound, betaaround, gammaround);
     $.ajax({
         type: "post",
         url: 'GyroDataSS.php',
-        data:{"alpha":alpharound,"beta":betaaround,"gamma":gammaround},
+        data:{"beta":betaaround,"gamma":gammaround},
                     success: function(data)
                     {
                         console.log(data);
@@ -53,7 +91,7 @@ function PHPRelay(ALPHA, BETA, GAMMA){
 
     });
 	
-	var dataoutput = function () {
+	/*var dataoutput = function () {
         $.ajax({
             type: 'POST',
             url: 'PullData.php',
@@ -69,12 +107,12 @@ function PHPRelay(ALPHA, BETA, GAMMA){
                 $('#alphaGraph').animate({height:data.alpha},40);
                 $('#betaGraph').animate({height:data.beta},40);
                 $('#gammaGraph').animate({height:data.gamma},40);
-                */
+              
             }
         });
         setTimeout(function () { dataoutput() }, 200);
     }
     dataoutput();
-	
+	  */
 }
 
--- GyroDataSS.php
+++ GyroDataSS.php
@@ -1,12 +1,17 @@
 <?php 
 echo "Data Loaded"; 
-$alpha = $_POST['alpha'];
 $beta = $_POST['beta'];
 $gamma = $_POST['gamma'];
 
 $file = "RCDATA.json";
-$json = array("alpha" => $alpha, "beta" => $beta, "gamma" => $gamma);
+$json = array("beta" => $beta, "gamma" => $gamma);
 
 file_put_contents($file, json_encode($json));
+
+$update = function($beta, $gamma){
+shell_exec("/var/www/rctest.cgi $beta $gamma");
+};
+
+$update($beta, $gamma);
 
 ?>
