8333201354473567f7cbcb42957ba8b84d5dd1d7
Keir Frampton <Keirframpton@gmail.com>
2014-03-03 23:13:11 +0000

    removed none required code

--- GyroControl.js
+++ GyroControl.js
@@ -90,29 +90,5 @@ function PHPRelay(BETA, GAMMA){
 		    }
 
     });
-	
-	/*var dataoutput = function () {
-        $.ajax({
-            type: 'POST',
-            url: 'PullData.php',
-            dataType: 'json',
-            cache: false,
-            success: function (data) {
-                console.log(data);
-                //$('#alpha').html('Alpha: ' + data.alpha);
-                //$('#beta').html('Beta: ' + data.beta);
-                //$('#gamma').html('Gamma: ' + data.gamma);
-                //document.getElementById("#alphaGraph").style.webkitTransform = "scale(10,"+ data.alpha +")";
-                /*
-                $('#alphaGraph').animate({height:data.alpha},40);
-                $('#betaGraph').animate({height:data.beta},40);
-                $('#gammaGraph').animate({height:data.gamma},40);
-              
-            }
-        });
-        setTimeout(function () { dataoutput() }, 200);
-    }
-    dataoutput();
-	  */
-}
 
+}
